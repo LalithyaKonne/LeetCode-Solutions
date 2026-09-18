8edb1ea1c97e2805ddb787b11ded331906ac40b1
Lalithya Konne <konnelalithya33@gmail.com>
2026-09-18 17:53:17 +0530

    Time: 32 ms (76.05%) | Memory: 60.5 MB (5.04%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -3,29 +3,31 @@ class Solution {
         List<List<Integer>> res=new ArrayList<>();
         Arrays.sort(a);
         int n=a.length;
-	  for(int i=0;i<n-2;i++)
-	  {
-        if (i>0 && a[i]==a[i-1]) continue;
-		int l=i+1,r=n-1;
-		while(l<r)
-		{
-			if(a[i]+a[l]+a[r]==0)
-			{
-				ArrayList<Integer> al=new ArrayList<>();
-				al.add(a[i]);
-				al.add(a[l]);
-				al.add(a[r]);
-				res.add(al);
-                while (l<r && a[l] == a[l + 1]) l++;
-                while (l<r && a[r] == a[r - 1]) r--;
-				l++;r--;
-			}
-			else if(a[i]+a[l]+a[r]<0)
-				l++;
-			else
-				r--;
-		}
-	  }
-		return res;
+        for(int i=0;i<n-2;i++)
+        {
+            if(i>0 && a[i]==a[i-1])
+                continue;
+            int l=i+1,r=n-1;
+            while(l<r)
+            {
+                if(a[i]+a[l]+a[r]==0)
+                {
+                    ArrayList<Integer> al=new ArrayList<>();
+                    al.add(a[i]);
+                    al.add(a[l]);
+                    al.add(a[r]);
+                    res.add(al);
+                    while(l<r && a[l]==a[l+1]) l++;
+                    while(l<r && a[r]==a[r-1]) r--;
+                    l++;
+                    r--;
+                }
+                else if(a[i]+a[l]+a[r]<0)
+                    l++;
+                else
+                    r--;
+            }
+        }
+        return res;
     }
 }
